5933bc5dede704f2528d43ea8d6153e7b06da878
doldre <779760807@qq.com>
2016-04-14 13:36:39 +0800

    cdoj 1312 dsu

---
+++ cdoj/1312_dsu.cpp
@@ -0,0 +1,43 @@
+/************************************************
+ *Author        :mathon
+ *Created Time  :2016年04月13日 星期三 19时04分04秒
+ *题目类型:
+*************************************************/
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <stack>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+typedef unsigned long long ull;
+vector<int> vi;
+#define xx first
+#define yy second
+#define sa(n) scanf("%d", &(n))
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define vep(c) for(decltype((c).begin() ) it = (c).begin(); it != (c).end(); it++) 
+#define pr(x) cout << #x << " " << x << " "
+#define prln(x) cout << #x << " " << x << endl
+const int mod = int(1e9) + 7, INF = 0x3f3f3f3f, maxn = 1e5 + 12;
+
+
+
+int main(void)
+{
+#ifdef LOCAL
+    //freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+#endif
+    
+    return 0;
+}
